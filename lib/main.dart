833d5a504d492397b900b54d895806c71b3901ff
Insoft Developers <irdn.software@gmail.com>
2026-02-02 15:49:41 +0700

    info dan main

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,8 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
 import 'package:flutter_inappwebview/flutter_inappwebview.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp]);
   runApp(const MyApp());
 }
 
@@ -15,47 +13,48 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return const MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: WebViewBootstrap(),
+      home: InitPage(),
     );
   }
 }
 
-/// ===============================
-/// BOOTSTRAP (ANTI STUCK iOS)
-/// ===============================
-class WebViewBootstrap extends StatefulWidget {
-  const WebViewBootstrap({super.key});
+class InitPage extends StatefulWidget {
+  const InitPage({super.key});
 
   @override
-  State<WebViewBootstrap> createState() => _WebViewBootstrapState();
+  State<InitPage> createState() => _InitPageState();
 }
 
-class _WebViewBootstrapState extends State<WebViewBootstrap> {
+class _InitPageState extends State<InitPage> {
   bool ready = false;
 
   @override
   void initState() {
     super.initState();
+
+    /// 🔥 TUNGGU FRAME PERTAMA SELESAI
     WidgetsBinding.instance.addPostFrameCallback((_) async {
       await Future.delayed(const Duration(milliseconds: 300));
-      if (mounted) setState(() => ready = true);
+      if (mounted) {
+        setState(() => ready = true);
+      }
     });
   }
 
   @override
   Widget build(BuildContext context) {
     if (!ready) {
+      // ⛑️ anti white screen
       return const Scaffold(
+        backgroundColor: Colors.white,
         body: Center(child: CircularProgressIndicator()),
       );
     }
+
     return const WebViewPage();
   }
 }
 
-/// ===============================
-/// WEBVIEW PAGE
-/// ===============================
 class WebViewPage extends StatefulWidget {
   const WebViewPage({super.key});
 
@@ -64,57 +63,28 @@ class WebViewPage extends StatefulWidget {
 }
 
 class _WebViewPageState extends State<WebViewPage> {
-  InAppWebViewController? _controller;
-  bool _error = false;
+  InAppWebViewController? controller;
 
   @override
   Widget build(BuildContext context) {
-    return WillPopScope(
-      onWillPop: () async {
-        if (_controller != null && await _controller!.canGoBack()) {
-          _controller!.goBack();
-          return false;
-        }
-        return true;
-      },
-      child: Scaffold(
-        body: SafeArea(
-          child: _error
-              ? const Center(
-                  child: Text(
-                    'Gagal memuat halaman.\nPeriksa koneksi internet.',
-                    textAlign: TextAlign.center,
-                  ),
-                )
-              : InAppWebView(
-                  initialUrlRequest: URLRequest(
-                    url: WebUri('https://anggotaperadi.or.id'),
-                  ),
-                  initialSettings: InAppWebViewSettings(
-                    // 🔥 WAJIB iOS
-                    useShouldOverrideUrlLoading: true,
-                    allowsInlineMediaPlayback: true,
-                    mediaPlaybackRequiresUserGesture: false,
-
-                    javaScriptEnabled: true,
-                    domStorageEnabled: true,
-                    databaseEnabled: true,
-                    cacheEnabled: true,
-
-                    // 🔥 PENTING untuk iOS putih
-                    allowsBackForwardNavigationGestures: true,
-                    useOnLoadResource: true,
-                  ),
-                  onWebViewCreated: (controller) {
-                    _controller = controller;
-                  },
-                  onLoadError: (controller, url, code, message) {
-                    setState(() => _error = true);
-                  },
-                  onLoadHttpError: (controller, url, statusCode, description) {
-                    setState(() => _error = true);
-                  },
-                ),
+    return Scaffold(
+      body: SafeArea(
+        child: InAppWebView(
+          initialUrlRequest: URLRequest(
+            url: WebUri('https://anggotaperadi.or.id'),
+          ),
+          initialSettings: InAppWebViewSettings(
+            javaScriptEnabled: true,
+            domStorageEnabled: true,
+            databaseEnabled: true,
+            cacheEnabled: true,
+            mediaPlaybackRequiresUserGesture: false,
+            allowsInlineMediaPlayback: true,
+            transparentBackground: false,
+          ),
+          onWebViewCreated: (c) {
+            controller = c;
+          },
         ),
       ),
     );
