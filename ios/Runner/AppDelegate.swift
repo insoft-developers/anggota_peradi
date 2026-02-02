967fd26fe9fd7fafc3dd545a11da1bca71434a02
Insoft Developers <irdn.software@gmail.com>
2026-02-02 13:39:52 +0700

    app delegate balik ke awal

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -1,21 +1,13 @@
-import UIKit
 import Flutter
+import UIKit
 
 @main
 @objc class AppDelegate: FlutterAppDelegate {
-
-
   override func application(
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
-
-    let flutterViewController = FlutterViewController()
-    window = UIWindow(frame: UIScreen.main.bounds)
-    window?.rootViewController = flutterViewController
-    window?.makeKeyAndVisible()
-
     GeneratedPluginRegistrant.register(with: self)
-    return true
+    return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
 }
