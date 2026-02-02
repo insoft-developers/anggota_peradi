31899f537837b048a4d06c6bad5f7598b955b129
Insoft Developers <irdn.software@gmail.com>
2026-02-02 13:13:23 +0700

    perbaikan delegate dan pod file

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -1,13 +1,21 @@
-import Flutter
 import UIKit
+import Flutter
 
 @main
 @objc class AppDelegate: FlutterAppDelegate {
+
+
   override func application(
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
+
+    let flutterViewController = FlutterViewController()
+    window = UIWindow(frame: UIScreen.main.bounds)
+    window?.rootViewController = flutterViewController
+    window?.makeKeyAndVisible()
+
     GeneratedPluginRegistrant.register(with: self)
-    return super.application(application, didFinishLaunchingWithOptions: launchOptions)
+    return true
   }
 }
